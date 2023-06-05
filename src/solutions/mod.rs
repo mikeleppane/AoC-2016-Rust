8ebe6303bc27e46b0ae3c8c266e8619791929bcb
Mikko Leppänen <mikko.leppanen@vaisala.com>
2023-06-05 14:55:52 +0300

    WIP: solution for day 7

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -6,6 +6,7 @@ use crate::solutions::day03::Day03;
 use crate::solutions::day04::Day04;
 use crate::solutions::day05::Day05;
 use crate::solutions::day06::Day06;
+use crate::solutions::day07::Day07;
 use crate::Selector;
 
 mod day01;
@@ -14,6 +15,7 @@ mod day03;
 mod day04;
 mod day05;
 mod day06;
+mod day07;
 
 pub fn run(which: Selector) {
     let mut day01 = Day01::new();
@@ -22,9 +24,10 @@ pub fn run(which: Selector) {
     let mut day04 = Day04::new();
     let mut day05 = Day05::new();
     let mut day06 = Day06::new();
+    let mut day07 = Day07::new();
 
     let mut days: Vec<&mut dyn Runner> = vec![
-        &mut day01, &mut day02, &mut day03, &mut day04, &mut day05, &mut day06,
+        &mut day01, &mut day02, &mut day03, &mut day04, &mut day05, &mut day06, &mut day07,
     ];
 
     match which {
